c64698f0ad3cd5220aacefd6327d81d47e0fc49e
Rune T. Larsen <rune@hjemmekino.no>
2025-12-19 22:00:56 +0100

    tailwind first iteration

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './themes/**/layouts/**/*.html',
+    './layouts/**/*.html',
+    './content/**/*.{html,md}',
+    './static/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'audi-blue': '#1eaef7',
+        'audi-blue-light': '#cff0ff',
+        'audi-blue-lighter': '#e3f7ff',
+        'audi-blue-dark': '#0062ba',
+        'audi-blue-darker': '#022f51',
+        'audi-purple': '#3f3161',
+        'audi-yellow': '#ffda06',
+        'audi-green': '#17c96b',
+        'audi-red': '#e23b53'
+      },
+      fontFamily: {
+        'din': ['DIN', 'sans-serif'],
+        'din-bold': ['DIN-bold', 'sans-serif']
+      },
+      maxWidth: {
+        '210px': '210px',
+        '120px': '120px',
+        '300px': '300px',
+        '336px': '336px'
+      }
+    }
+  },
+  plugins: []
+}
